4054abe0fb3da4fe1a7c15c0f0a922ab6cfdc2be
njine <brikab@gmail.com>
2024-04-23 02:28:53 +0300

    zer

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,25 +1,10 @@
 #!/usr/bin/env python3
-'''Module for measuring the runtime of asynchronous tasks.'''
-
+'''Task 0's module for defining wait_random function.'''
 import asyncio
-import time
+import random
 
-# Importing the wait_n coroutine
-wait_n = __import__('1-concurrent_coroutines').wait_n
-
-def measure_time(n: int, max_delay: int) -> float:
-    '''Computes the average runtime of wait_n with given parameters.
-
-    Parameters:
-    n (int): The number of times to run wait_n.
-    max_delay (int): The maximum delay for each wait_random coroutine.
-
-    Returns:
-    float: The average runtime for wait_n(n, max_delay).
-    '''
-    # Get the start time
-    start_time = time.time()
-    # Run the asynchronous function
-    asyncio.run(wait_n(n, max_delay))
-    # Calculate the elapsed time and return the average
-    return (time.time() - start_time) / n
+async def wait_random(max_delay: int = 10) -> float:
+    '''Waits for a random number of seconds and returns the delay.'''
+    delay = random.random() * max_delay
+    await asyncio.sleep(delay)
+    return delay
